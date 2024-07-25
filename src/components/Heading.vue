cb3ae5182d8c4087ae590d6cf8d6dd61e7533158
Felipse <samborskaya.r@yandex.ru>
2024-07-25 16:43:18 +0300

    add heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="heading">
+    <div class="container">
+      <div class="heading__wrap heading__wrap--center">
+        <div class="heading__title">{{ content.title }}</div>
+        <div class="heading__text">
+          {{ content.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- .heading -->
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  content: {
+    title: string;
+    text: string;
+  };
+}>();
+</script>
+
+<style scoped></style>
